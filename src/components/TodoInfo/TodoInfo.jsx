4cbb003014e4549a6ade024835ffa13a307609a4
Nikita Bedenko <nikitabedenko@gmail.com>
2025-11-15 06:29:47 +0200

    work in progress

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,12 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo: { id, userId, completed, title } }) => {
+  return (
+    <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">HTML</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+  );
+};
